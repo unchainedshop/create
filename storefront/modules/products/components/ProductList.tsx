0cae1228f070be29fd9ee2cb68b9bd9c26ab329a
Simon Emanuel Schmid <simon@schmid.io>
2020-04-03 18:02:05 +0200

    Feat: Product list with meta data

--- storefront/modules/products/components/ProductList.tsx
+++ storefront/modules/products/components/ProductList.tsx
@@ -1,18 +1,28 @@
 import useProductsQuery from '../hooks/useProductsQuery';
 
+const getProductMediaUrl = (product) => {
+  const media = product?.media;
+
+  const firstMedia = media[0];
+
+  return firstMedia?.file?.url;
+};
+
 const ProductList = () => {
   const { products } = useProductsQuery();
 
   return (
     <div className="row">
       <div className="col-md-6 col-lg-4">
-        {products.map(product => (
-          <div>
-            <img src="static/img/Auberginen-Curry.jpg" alt="" />
+        {products.map((product) => (
+          <div key={product._id}>
+            <h2>{product?.texts?.title}</h2>
+            <img src={getProductMediaUrl(product)} />
             <div className="p-2">
-              {product._id}
-              <h4 className="mb-0">title</h4>
-              <p>desc</p>
+              <h4 className="mb-0">{product?.texts?.subtitle}</h4>
+              <p>{product?.texts?.description}</p>
+              <p>{product?.dimensions?.weight * 100} Gramm</p>
+              <p>{product?.simulatedPrice?.price?.amount / 100} CHF</p>
             </div>
           </div>
         ))}

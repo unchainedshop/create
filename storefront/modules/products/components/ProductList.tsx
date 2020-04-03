2ce98eecaa95452c5708ba7f2d8124bce990e21b
vedi <vedran@unchained.shop>
2020-04-03 17:56:32 +0200

    add basic product-list styles

--- storefront/modules/products/components/ProductList.tsx
+++ storefront/modules/products/components/ProductList.tsx
@@ -4,10 +4,19 @@ const ProductList = () => {
   const { products } = useProductsQuery();
 
   return (
-    <div>
-      {products.map((product) => (
-        <div>{product._id}</div>
-      ))}
+    <div className="row">
+      <div className="col-md-6 col-lg-4">
+        {products.map(product => (
+          <div>
+            <img src="static/img/Auberginen-Curry.jpg" alt="" />
+            <div className="p-2">
+              {product._id}
+              <h4 className="mb-0">title</h4>
+              <p>desc</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };

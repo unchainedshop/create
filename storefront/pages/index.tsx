5d4f4fd6d7046cf65654a00014325bede092f404
vedi <vedran@unchained.shop>
2020-04-03 23:11:25 +0200

    add styles for login / cart

--- storefront/pages/index.tsx
+++ storefront/pages/index.tsx
@@ -4,36 +4,41 @@ import ProductList from '../modules/products/components/ProductList';
 import LoginCart from '../modules/auth/components/LoginCart';
 
 const Home = () => (
-  <div className="container">
+  <div>
     <Head>
       <title>Currybag - Innerhalb von 5 Minuten ready!</title>
     </Head>
 
-    <LoginCart />
-
-    <header className="header text-center pt-5">
-      <div className="row">
-        <div className="col-lg-8 offset-lg-2">
-          <div className="font-heading">LOVING LANKA®</div>
-          <h1 className="title color-brand">Currybag™</h1>
-          <p>
-            <b>Was ist ein Currybag?</b>
-          </p>
-          <p>
-            Ganz einfach: Das ist ein Beutel und darin befindet sich ein
-            hausgemachtes Curry aus dem Hause Loving Lanka.{' '}
-            <a className="link" href="https://www.lovinglanka.ch">
-              www.lovinglanka.ch
-            </a>
-          </p>
-          <img
-            src="static/img/freigestellt-1494x2011.png"
-            alt="karrotten curry"
-          />
-        </div>
+    <div className="container py-2">
+      <LoginCart />
+    </div>
+    <div className="color-bg-brand-lightest">
+      <div className="container">
+        <header className="header text-center pt-5">
+          <div className="row">
+            <div className="col-lg-8 offset-lg-2">
+              <div className="font-heading">LOVING LANKA®</div>
+              <h1 className="title color-brand">Currybag™</h1>
+              <p>
+                <b>Was ist ein Currybag?</b>
+              </p>
+              <p>
+                Ganz einfach: Das ist ein Beutel und darin befindet sich ein
+                hausgemachtes Curry aus dem Hause Loving Lanka.{' '}
+                <a className="link" href="https://www.lovinglanka.ch">
+                  www.lovinglanka.ch
+                </a>
+              </p>
+              <img
+                src="static/img/freigestellt-1494x2011.png"
+                alt="karrotten curry"
+              />
+            </div>
+          </div>
+        </header>
       </div>
-    </header>
-    <main className="my-5">
+    </div>
+    <main className="conntainer my-5">
       <ProductList />
     </main>
     <footer className="footer"></footer>

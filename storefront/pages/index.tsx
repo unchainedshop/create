199464f03f77fef777b8b90c6d6e9ffb0dd57854
vedi <vedran@unchained.shop>
2020-04-03 17:01:29 +0200

    add favicon & barlow google font

--- storefront/pages/index.tsx
+++ storefront/pages/index.tsx
@@ -5,16 +5,20 @@ import ProductList from "../modules/products/components/ProductList";
 const Home = () => (
   <div className="container">
     <Head>
-      <title>Currybag</title>
+      <title>Currybag - Innerhalb von 5 Minuten ready!</title>
     </Head>
 
-    <main>
-      <h1 className="title">Currybag!</h1>
+    <header className="header text-center pt-3">
+      <div className="font-heading">LOVING LANKA®</div>
+      <h1 className="title color-brand">Currybag™</h1>
       <button role="button" className="button button--primary">
         primary button
       </button>
+    </header>
+    <main className="my-5">
       <ProductList />
     </main>
+    <footer className="footer"></footer>
   </div>
 );
 

f5aa3a7755145deb2279fddbcbc21b36ffd60198
vedi <vedran@unchained.shop>
2020-04-03 19:51:47 +0200

    add basic product detail styles

--- storefront/pages/curry/[slug].tsx
+++ storefront/pages/curry/[slug].tsx
@@ -8,16 +8,28 @@ const Detail = (props) => {
   const { product } = useProductDetailQuery({ slug: router.query.slug });
 
   return (
-    <div className="col-md-6 col-lg-4 product-list-item">
-      <h2 className="px-2">{product?.texts?.title}</h2>
-      <img src={getProductMediaUrl(product)} />
-      <div className="p-2">
-        <h4 className="my-0">
-          CHF {product?.simulatedPrice?.price?.amount / 100}.-
-        </h4>
-        <h4 className="mb-0">{product?.texts?.subtitle}</h4>
-        <p>{product?.texts?.description}</p>
-        <p>{product?.dimensions?.weight * 1000} Gramm</p>
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-6">
+          <img src={getProductMediaUrl(product)} />
+        </div>
+        <div className="col-md-6">
+          <h2 className="px-2 mt-md-0">{product?.texts?.title}</h2>
+          <div className="p-2">
+            <h3 className="my-0">
+              CHF {product?.simulatedPrice?.price?.amount / 100}.-
+            </h3>
+            <h4 className="mb-0">{product?.texts?.subtitle}</h4>
+            <p>{product?.texts?.description}</p>
+            <p>{product?.dimensions?.weight * 1000} Gramm</p>
+          </div>
+          <button
+            type="button"
+            className="button button--primary button--big  text-uppercase"
+          >
+            In den Warenkorb
+          </button>
+        </div>
       </div>
     </div>
   );

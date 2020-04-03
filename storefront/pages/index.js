6f1c5e937c38370de2bc71158b2a73e4903dc373
Simon Emanuel Schmid <simon@schmid.io>
2020-04-03 15:09:17 +0200

    Feat: Basic next.js setup

---
+++ storefront/pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const Home = () => (
+  <div className="container">
+    <Head>
+      <title>Currybag</title>
+    </Head>
+
+    <main>
+      <h1 className="title">Currybag!</h1>
+    </main>
+  </div>
+);
+
+export default Home;

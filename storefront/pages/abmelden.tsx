4719ee5f036c5be242c5d711631726a78c153cf6
Simon Emanuel Schmid <simon@schmid.io>
2020-04-03 21:19:48 +0200

    Feat: Logout

---
+++ storefront/pages/abmelden.tsx
@@ -0,0 +1,21 @@
+import useLogoutMutation from '../modules/auth/hooks/useLogoutMutation';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+const Logout = () => {
+  const { logout } = useLogoutMutation();
+  const router = useRouter();
+
+  useEffect(() => {
+    (async () => {
+      await logout();
+      // router.push('/');
+      // HACK
+      window.location.href = '/';
+    })();
+  }, []);
+
+  return <div>Du wirst abgemeldet</div>;
+};
+
+export default Logout;

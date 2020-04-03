65fe7b318955d6e6f9f9ba476c9cce8cd2a0ea38
vedi <vedran@unchained.shop>
2020-04-03 18:47:49 +0200

    add basic product-list styles

--- storefront/modules/products/components/ProductList.tsx
+++ storefront/modules/products/components/ProductList.tsx
@@ -1,6 +1,6 @@
 import useProductsQuery from '../hooks/useProductsQuery';
 
-const getProductMediaUrl = (product) => {
+const getProductMediaUrl = product => {
   const media = product?.media;
 
   const firstMedia = media[0];
@@ -13,20 +13,20 @@ const ProductList = () => {
 
   return (
     <div className="row">
-      <div className="col-md-6 col-lg-4">
-        {products.map((product) => (
-          <div key={product._id}>
-            <h2>{product?.texts?.title}</h2>
-            <img src={getProductMediaUrl(product)} />
-            <div className="p-2">
-              <h4 className="mb-0">{product?.texts?.subtitle}</h4>
-              <p>{product?.texts?.description}</p>
-              <p>{product?.dimensions?.weight * 100} Gramm</p>
-              <p>{product?.simulatedPrice?.price?.amount / 100} CHF</p>
-            </div>
+      {products.map(product => (
+        <div className="col-md-6 col-lg-4 product-list-item" key={product._id}>
+          <h2 className="px-2">{product?.texts?.title}</h2>
+          <img src={getProductMediaUrl(product)} />
+          <div className="p-2">
+            <h4 className="my-0">
+              CHF {product?.simulatedPrice?.price?.amount / 100}.-
+            </h4>
+            <h4 className="mb-0">{product?.texts?.subtitle}</h4>
+            <p>{product?.texts?.description}</p>
+            <p>{product?.dimensions?.weight * 1000} Gramm</p>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
   );
 };

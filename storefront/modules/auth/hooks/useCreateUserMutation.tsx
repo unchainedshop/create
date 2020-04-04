20f9a9c35fe47850fd79c0e298c8e41a0f470501
Simon Emanuel Schmid <simon@schmid.io>
2020-04-04 18:27:50 +0200

    Feat: Proper transform guestUser to registered user

--- storefront/modules/auth/hooks/useCreateUserMutation.tsx
+++ storefront/modules/auth/hooks/useCreateUserMutation.tsx
@@ -1,18 +1,37 @@
 import gql from 'graphql-tag';
 import { useMutation } from '@apollo/react-hooks';
 
+import CurrentUserFragment from '../fragments/CurrentUserFragment';
+import { UserQuery } from './useUserQuery';
+
 const CreateUserMutation = gql`
   mutation CreateUser($email: String!, $password: String!) {
     createUser(email: $email, plainPassword: $password) {
       id
       token
+      user {
+        ...CurrentUserFragment
+      }
     }
   }
+  ${CurrentUserFragment}
 `;
 
 const useCreateUserMutation = () => {
   const [createUserMutation, { error }] = useMutation(CreateUserMutation, {
-    refetchQueries: ['UserQuery'],
+    // refetchQueries: ['UserQuery'],
+    update(cache, result) {
+      // const { todos } = cache.readQuery({ query: GET_TODOS });
+      const newUser = result?.data?.createUser?.user;
+
+      if (newUser) {
+        cache.writeQuery({
+          query: UserQuery,
+          data: { me: newUser },
+        });
+      }
+      console.log(cache, result);
+    },
   });
 
   const createUser = async ({ email, password }) => {

da34885f821c590d6612fddc162983c29c8e5658
vedi <vedran@unchained.shop>
2020-04-04 13:15:28 +0200

    add more styles

---
+++ storefront/pages/anmelden.jsx
@@ -0,0 +1,28 @@
+import Head from 'next/head';
+
+const SignIn = () => (
+  <div className="container">
+    <div className="row">
+      <div className="col-md-8 offset-md-2">
+        <h1>Anmeldung</h1>
+        <form className="form">
+          <div className="form-row">
+            <div className="mb-3 col-md-6">
+              <label className="form-label">Deine E-Mail Adresse</label>
+              <input className="form-control" name="email" />
+            </div>
+            <div className="mb-3 col-md-6">
+              <label className="form-label">Dein Passwort</label>
+              <input className="form-control" name="password" />
+            </div>
+          </div>
+          <button className="button button--primary" type="submit">
+            Anmelden
+          </button>
+        </form>
+      </div>
+    </div>
+  </div>
+);
+
+export default SignIn;

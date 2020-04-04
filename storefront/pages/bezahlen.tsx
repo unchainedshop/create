35cf62ad3ca744a88301c5e1ad557fa7ab48ead7
Simon Emanuel Schmid <simon@schmid.io>
2020-04-04 17:07:39 +0200

    Feat: Placeholder for Billing section

--- storefront/pages/bezahlen.tsx
+++ storefront/pages/bezahlen.tsx
@@ -5,9 +5,21 @@ import ManageCart from '../modules/cart/components/ManageCart';
 import useUserQuery from '../modules/auth/hooks/useUserQuery';
 import useUpdateCartMutation from '../modules/checkout/hooks/useUpdateCartMutation';
 
-const EditableField = ({ register, name, value, isEditing }) => {
+const EditableField = ({
+  register,
+  name,
+  value,
+  isEditing,
+  type = 'text',
+  required = false,
+}) => {
   return isEditing ? (
-    <input type="text" name={name} defaultValue={value} ref={register} />
+    <input
+      type={type}
+      name={name}
+      defaultValue={value}
+      ref={register({ required })}
+    />
   ) : (
     <div>{value}</div>
   );
@@ -26,9 +38,12 @@ const DeliverySection = () => {
     addressLine,
     postalCode,
     city,
+    emailAddress,
+    telNumber,
   }) => {
     if (isEditing) {
       await updateCart({
+        contact: { emailAddress, telNumber },
         billingAddress: {
           firstName,
           lastName,
@@ -39,7 +54,6 @@ const DeliverySection = () => {
         },
       });
 
-      console.log('finished');
       setEditing(false);
     } else {
       setEditing(true);
@@ -47,19 +61,31 @@ const DeliverySection = () => {
   };
 
   const addressFields = [
-    { name: 'firstName', translation: 'Vorname' },
-    { name: 'lastName', translation: 'Nachname' },
-    { name: 'company', translation: 'Firma' },
-    { name: 'addressLine', translation: 'Adresse' },
-    { name: 'postalCode', translation: 'PLZ' },
-    { name: 'city', translation: 'Ort' },
+    { name: 'firstName', translation: 'Vorname', type: 'text', required: true },
+    { name: 'lastName', translation: 'Nachname', type: 'text', required: true },
+    { name: 'company', translation: 'Firma', type: 'text', required: false },
+    {
+      name: 'addressLine',
+      translation: 'Adresse',
+      type: 'text',
+      required: true,
+    },
+    { name: 'postalCode', translation: 'PLZ', type: 'text', required: true },
+    { name: 'city', translation: 'Ort', type: 'text', required: true },
+    {
+      name: 'emailAddress',
+      translation: 'Email',
+      type: 'email',
+      required: true,
+    },
+    { name: 'telNumber', translation: 'Telefon', type: 'text', required: true },
   ];
 
   return (
     <form className="form" onSubmit={handleSubmit(onSubmit)}>
       <table>
         <tbody>
-          {addressFields.map(({ name, translation }) => (
+          {addressFields.map(({ name, translation, type, required }) => (
             <tr key={name}>
               <td>
                 <b>{translation}</b>
@@ -67,9 +93,14 @@ const DeliverySection = () => {
               <td>
                 <EditableField
                   name={name}
-                  value={user?.cart?.billingAddress?.[name]}
+                  value={
+                    user?.cart?.billingAddress?.[name] ||
+                    user?.cart?.contact?.[name]
+                  }
                   register={register}
                   isEditing={isEditing}
+                  type={type}
+                  required={required}
                 />
               </td>
             </tr>
@@ -79,6 +110,112 @@ const DeliverySection = () => {
       <button className="button button--primary" type="submit">
         {isEditing ? 'Speichern' : 'Ändern'}
       </button>
+    </form>
+  );
+};
+
+const BillingSection = () => {
+  const { user } = useUserQuery();
+  const [isEditing, setEditing] = useState(false);
+  const { updateCart } = useUpdateCartMutation();
+  const { register, handleSubmit, watch } = useForm();
+
+  const sameAsDelivery = watch('same');
+
+  const onSubmit = async ({
+    firstName,
+    lastName,
+    company,
+    addressLine,
+    postalCode,
+    city,
+  }) => {
+    if (isEditing) {
+      // await updateCart({
+      //   contact: { emailAddress, telNumber },
+      //   billingAddress: {
+      //     firstName,
+      //     lastName,
+      //     company,
+      //     addressLine,
+      //     postalCode,
+      //     city,
+      //   },
+      // });
+
+      console.log({
+        firstName,
+        lastName,
+        company,
+        addressLine,
+        postalCode,
+        city,
+      });
+
+      setEditing(false);
+    } else {
+      setEditing(true);
+    }
+  };
+
+  const addressFields = [
+    { name: 'firstName', translation: 'Vorname', type: 'text', required: true },
+    { name: 'lastName', translation: 'Nachname', type: 'text', required: true },
+    { name: 'company', translation: 'Firma', type: 'text', required: false },
+    {
+      name: 'addressLine',
+      translation: 'Adresse',
+      type: 'text',
+      required: true,
+    },
+    { name: 'postalCode', translation: 'PLZ', type: 'text', required: true },
+    { name: 'city', translation: 'Ort', type: 'text', required: true },
+  ];
+
+  return (
+    <form className="form" onSubmit={handleSubmit(onSubmit)}>
+      <input
+        type="checkbox"
+        className="form-check-input"
+        id="same"
+        name="same"
+        ref={register}
+        defaultChecked
+      />
+      <label className={`form-check-label mb-0`} htmlFor="same">
+        Gleich wie Lieferadresse
+      </label>
+
+      {!sameAsDelivery ? (
+        <>
+          <table>
+            <tbody>
+              {addressFields.map(({ name, translation, type, required }) => (
+                <tr key={name}>
+                  <td>
+                    <b>{translation}</b>
+                  </td>
+                  <td>
+                    <EditableField
+                      name={name}
+                      value={user?.cart?.billingAddress?.[name]}
+                      register={register}
+                      isEditing={isEditing}
+                      type={type}
+                      required={required}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <button className="button button--primary" type="submit">
+            {isEditing ? 'Speichern' : 'Ändern'}
+          </button>
+        </>
+      ) : (
+        ''
+      )}
     </form>
   );
 };
@@ -96,7 +233,7 @@ const Payment = () => {
           <DeliverySection />
 
           <h2>Rechnungsadresse</h2>
-          <p>Gleich wie Lieferadresse</p>
+          <BillingSection />
         </div>
       </div>
     </div>

4de8b9272794db3addf1a80e78a699f06168d97a
Pascal Kaufmann <kaufmae@me.com>
2020-04-04 20:46:38 +0200

    add CHF 8.00 fee

--- engine/lib/plugins/index.js
+++ engine/lib/plugins/index.js
@@ -3,6 +3,9 @@ import 'meteor/unchained:core-payment/plugins/invoice';
 import 'meteor/unchained:core-delivery/plugins/send-mail';
 import 'meteor/unchained:core-pricing/plugins/order-items';
 import 'meteor/unchained:core-pricing/plugins/order-discount';
+import 'meteor/unchained:core-pricing/plugins/order-delivery';
+import 'meteor/unchained:core-pricing/plugins/delivery-swiss-tax';
+import 'meteor/unchained:core-pricing/plugins/product-swiss-tax';
 import 'meteor/unchained:core-pricing/plugins/product-catalog-price';
 import 'meteor/unchained:core-pricing/plugins/product-discount';
 import 'meteor/unchained:core-filters/plugins/local-search';
@@ -12,6 +15,7 @@ import { WorkerDirector } from 'meteor/unchained:core-worker';
 import EventListenerWorker from 'meteor/unchained:core-worker/workers/eventListener';
 import FailedRescheduler from 'meteor/unchained:core-worker/schedulers/failedRescheduler';
 import './author-filter';
+import './delivery-price';
 
 export default () => {
   const { DISABLE_WORKER } = process.env;

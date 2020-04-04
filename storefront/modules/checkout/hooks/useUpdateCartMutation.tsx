d75e3488d8fb3a95e1c0c28448a2771ff99cd39a
Simon Emanuel Schmid <simon@schmid.io>
2020-04-04 14:47:52 +0200

    Feat: Add address to cart

---
+++ storefront/modules/checkout/hooks/useUpdateCartMutation.tsx
@@ -0,0 +1,24 @@
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/react-hooks';
+
+const UpdateCartMutation = gql`
+  mutation UpdateCart($billingAddress: AddressInput, $contact: ContactInput) {
+    updateCart(billingAddress: $billingAddress, contact: $contact) {
+      _id
+    }
+  }
+`;
+
+const useUpdateCartMutation = () => {
+  const [updateCartMutation] = useMutation(UpdateCartMutation);
+
+  const updateCart = async ({ contact, billingAddress }) => {
+    return await updateCartMutation({ variables: { contact, billingAddress } });
+  };
+
+  return {
+    updateCart,
+  };
+};
+
+export default useUpdateCartMutation;

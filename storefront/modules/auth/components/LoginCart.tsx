181413e673a5d0a4ed162636d02f5cabd6f10ba0
vedi <vedran@unchained.shop>
2020-04-04 16:37:57 +0200

    improve error-states

--- storefront/modules/auth/components/LoginCart.tsx
+++ storefront/modules/auth/components/LoginCart.tsx
@@ -7,7 +7,7 @@ const LoginCart = () => {
   return user ? (
     <div className="d-flex justify-content-end align-items-center flex-wrap">
       <Link href="/warenkorb">
-        <a className="link my-2">
+        <a className="button button--primary my-2">
           {user?.cart?.items.length} Produkte im Warenkorb für insgesamt{' '}
           {(user?.cart?.total?.amount / 100).toFixed(2)} CHF
         </a>
--- storefront/pages/warenkorb.tsx
+++ storefront/pages/warenkorb.tsx
@@ -15,39 +15,30 @@ const Cart = () => {
   return (
     <div className="container">
       <Header />
-      <div className="row">
-        <div className="col-md-8 offset-md-2">
-          <h1>Warenkorb</h1>
-          <ManageCart />
+      <h1>Warenkorb</h1>
+      <ManageCart />
 
-          <div className="mb-1">
-            <Link
-              href={
-                user?.isGuest ?? true
-                  ? '/registrieren?next=bezahlen'
-                  : '/bezahlen'
-              }
-            >
-              <a
-                className="button button--primary button--big  text-uppercase"
-                // onClick={handleClick}
-              >
-                Einmalig Bestellen
-              </a>
-            </Link>
-          </div>
-
-          <div>
-            <button
-              disabled
-              type="button"
-              className="button button--primary button--big  text-uppercase"
-              // onClick={handleClick}
-            >
-              Im Abo Bestellen
-            </button>
-          </div>
-        </div>
+      <div className="button-group mt-5">
+        <Link
+          href={
+            user?.isGuest ?? true ? '/registrieren?next=bezahlen' : '/bezahlen'
+          }
+        >
+          <a
+            className="button button--primary button--big  text-uppercase"
+            // onClick={handleClick}
+          >
+            Einmalig Bestellen
+          </a>
+        </Link>
+        <button
+          disabled
+          type="button"
+          className="button button--primary button--big  text-uppercase"
+          // onClick={handleClick}
+        >
+          Im Abo Bestellen
+        </button>
       </div>
     </div>
   );

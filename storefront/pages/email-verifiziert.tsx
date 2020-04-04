b4b09984dc27fd2be16bd3d891d08d6fdd97d2d8
vedi <vedran@unchained.shop>
2020-04-04 22:59:23 +0200

    add verified email page

---
+++ storefront/pages/email-verifiziert.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+import CheckoutFooter from '../modules/layout/components/CheckoutFooter';
+
+const VerifiedEmail = () => {
+  return (
+    <div className="container">
+      <Link href="/">
+        <a className="color-brand">
+          <h3 className="my-2 mr-2">Currybag™</h3>
+        </a>
+      </Link>
+      <div className="row">
+        <div className="col-md-8 offset-md-2">
+          <h1>Deine E-Mail ist erfolgreich verifiziert worden</h1>
+          <p>...</p>
+        </div>
+      </div>
+      <CheckoutFooter />
+    </div>
+  );
+};
+
+export default VerifiedEmail;

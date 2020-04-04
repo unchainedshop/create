066c3ff7dce3fd209774d952874930ce422df7b5
Pascal Kaufmann <kaufmae@me.com>
2020-04-04 20:30:33 +0200

    delivery plugin: use send-mail

--- engine/lib/seed/delivery.config.js
+++ engine/lib/seed/delivery.config.js
@@ -3,8 +3,8 @@ import { DeliveryProviderType } from 'meteor/unchained:core-delivery';
 export default {
   deliveryProviders: [
     {
-      id: 'manual',
-      adapterKey: 'shop.unchained.manual',
+      id: 'send-mail',
+      adapterKey: 'shop.unchained.send-mail',
       type: DeliveryProviderType.DELIVERY,
     },
   ],

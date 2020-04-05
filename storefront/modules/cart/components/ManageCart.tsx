2d90719649cd18123f10e2deb66b391ffa5417cc
vedi <vedran@unchained.shop>
2020-04-05 15:59:09 +0200

    improve cart style

--- storefront/modules/cart/components/ManageCart.tsx
+++ storefront/modules/cart/components/ManageCart.tsx
@@ -9,36 +9,35 @@ const ManageCart = () => {
   const handleRemoveClick = (itemId) => () => removeCartItem({ itemId });
 
   return (
-    <table>
-      <tbody>
-        {(user?.cart?.items || []).map((item) => (
-          <tr key={item._id}>
-            <td>{item.quantity} x</td>
-            {/* <td>
-                    <img src={getProductMediaUrl(item.product)} />
-                  </td> */}
-            <td>{item.product.texts.title}</td>
-            <td>{renderPrice(item.total)}</td>
-            <td>
-              <button
-                className="no-button"
-                type="button"
-                onClick={handleRemoveClick(item._id)}
-              >
-                ❌ Entfernen
-              </button>
-            </td>
-          </tr>
-        ))}
-        <tr className="border-top">
-          <td></td>
-          <td>Total</td>
-          <td>
-            <b>{renderPrice(user?.cart?.total)}</b>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div>
+      {(user?.cart?.items || []).map((item) => (
+        <div
+          className="d-flex justify-content-between flex-wrap border-top py-2"
+          key={item._id}
+        >
+          <div className="cart-img mr-2">
+            <img src={getProductMediaUrl(item.product)} />
+          </div>
+          <div className="w-50 mr-2">
+            <div>{item.quantity} x</div>
+            <div>{item.product.texts.title}</div>
+          </div>
+          <div className="align-self-end">{renderPrice(item.total)}</div>
+          <button
+            className="no-button align-self-start ml-2"
+            type="button"
+            onClick={handleRemoveClick(item._id)}
+          >
+            🗑️
+          </button>
+        </div>
+      ))}
+      <div className="text-right">
+        <h3 className="border-top pt-3 mt-0">
+          {renderPrice(user?.cart?.total)}
+        </h3>
+      </div>
+    </div>
   );
 };
 

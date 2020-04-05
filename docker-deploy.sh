09a4e96b48647b54db91a1b7cf31debfc1f38722
Pascal Kaufmann <kaufmae@me.com>
2020-04-05 15:54:48 +0200

    fix traefik issue

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,3 +1,3 @@
 export $(cat .env | xargs)
 docker-compose -f docker-compose.production.yml build
-docker stack deploy -c docker-compose.production.yml udf
+docker stack deploy -c docker-compose.production.yml --resolve-image never udf

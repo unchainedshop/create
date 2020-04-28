3c38eb1c3319075f101c482cb62d64c61731d1a3
thomasroth <thoemu.roth@gmail.com>
2020-04-28 13:25:19 +0200

    added possibilty to order directly on the overview list

--- storefront/modules/products/components/ProductList.tsx
+++ storefront/modules/products/components/ProductList.tsx
@@ -1,32 +1,47 @@
 import Link from 'next/link';
 
+import useConditionalAddCartProductMutation from '../../cart/hooks/useConditionalAddCartProductMutation';
+
 import useProductListQuery from '../hooks/useProductListQuery';
 import getProductMediaUrl from '../utils/getProductMediaUrl';
 
 const ProductList = () => {
   const { products } = useProductListQuery();
+  const { conditionalAddCartProduct } = useConditionalAddCartProductMutation();
+
+  const handleClick = (productId) => {
+    conditionalAddCartProduct({ productId });
+  };
 
   return (
     <div className="container">
       <div className="row">
         {products.map((product) => (
-          <Link
+          <div
             key={product._id}
-            href="/curry/[slug]"
-            as={`/curry/${product.texts.slug}`}
+            className="col-sm-6 col-lg-4 product-list-item"
           >
-            <a className="col-sm-6 col-lg-4 product-list-item">
-              <h3 className="px-2 my-3">{product?.texts?.title}</h3>
-              <img src={getProductMediaUrl(product)} />
-              <div className="p-2">
-                <h4 className="my-0">
-                  CHF {product?.simulatedPrice?.price?.amount / 100}.-
-                </h4>
-                <h4 className="mb-0">{product?.texts?.subtitle}</h4>
-                <p>{product?.texts?.description?.split('\n')[0]}</p>
-              </div>
-            </a>
-          </Link>
+            <Link href="/curry/[slug]" as={`/curry/${product.texts.slug}`}>
+              <a>
+                <h3 className="px-2 my-3">{product?.texts?.title}</h3>
+                <img src={getProductMediaUrl(product)} />
+                <div className="p-2">
+                  <h4 className="my-0">
+                    CHF {product?.simulatedPrice?.price?.amount / 100}.-
+                  </h4>
+                  <h4 className="mb-0">{product?.texts?.subtitle}</h4>
+                  <p>{product?.texts?.description?.split('\n')[0]}</p>
+                </div>
+              </a>
+            </Link>
+            <button
+              type="button"
+              className="button button--primary button--big mb-5 text-uppercase"
+              onClick={() => handleClick(product._id)}
+            >
+              In den Warenkorb
+            </button>
+          </div>
         ))}
       </div>
     </div>

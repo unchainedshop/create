11cf42aa6bd8a73b9699bb3115581dca57a4eb18
Pascal Kaufmann <kaufmae@me.com>
2020-05-07 22:51:59 +0200

    fix: compatibility with unchained 0.47.0

--- engine/lib/plugins/index.js
+++ engine/lib/plugins/index.js
@@ -12,21 +12,5 @@ import 'meteor/unchained:core-pricing/plugins/product-discount';
 import 'meteor/unchained:core-filters/plugins/local-search';
 import 'meteor/unchained:core-worker/plugins/external';
 import 'meteor/unchained:core-worker/plugins/heartbeat';
-import { WorkerDirector } from 'meteor/unchained:core-worker';
-import EventListenerWorker from 'meteor/unchained:core-worker/workers/eventListener';
-import FailedRescheduler from 'meteor/unchained:core-worker/schedulers/failedRescheduler';
 import './author-filter';
 import './delivery-price';
-
-export default () => {
-  const { DISABLE_WORKER } = process.env;
-
-  new FailedRescheduler({ WorkerDirector }).start();
-
-  if (!DISABLE_WORKER) {
-    new EventListenerWorker({
-      WorkerDirector,
-      workerId: 'EventWorker',
-    }).start();
-  }
-};

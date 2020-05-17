f2cb8751b5e6d45b592ae08e089feb021ad13955
mikearaya <mikaelaraya12@gmail.com>
2020-05-17 15:59:50 +0300

    Update order list view

--- storefront/modules/orders/components/OrderList.tsx
+++ storefront/modules/orders/components/OrderList.tsx
@@ -3,18 +3,42 @@ import Link from 'next/link';
 import useOrderListQuery from '../hooks/useUserOrderListQuery';
 
 const OrderList = () => {
-  const { orders } = useOrderListQuery();
+  const { orders, loading } = useOrderListQuery();
+
+  if (loading) return <p> loading .... </p>;
 
   return (
     <div className="container">
       <div className="row">
         {orders.map((order) => (
-          <div className="col-sm-6" key={order._id}>
+          <div className="col-sm-12" key={order._id}>
             <Link href="/order/[id]" as={`/order/${order._id}`}>
               <a className="card">
-                <h5 className="card-header">{order._id}</h5>
+                <h5 className="card-header">
+                  Order Number: {order.orderNumber}
+                </h5>
                 <div className="card-body">
-                  <h5 className="card-title">{order.created}</h5>
+                  <h5 className="card-title">
+                    Created: {new Date(order.created).toISOString()}
+                  </h5>
+                  <dl>
+                    <dt>Delivery type</dt>
+                    <dd>{order.delivery.provider.type}</dd>
+                    <dt>Payment type</dt>
+                    <dd>{order.supportedPaymentProviders[0].type}</dd>
+
+                    <dt>Payment status</dt>
+                    <dd>{order.payment.status}</dd>
+                    <dt>Total</dt>
+                    <dd>
+                      {order.total.amount} {order.total.currency}
+                    </dd>
+                    <dt>Country</dt>
+                    <dd>
+                      {order.country.name}
+                      <span>{order.country.flagEmoji}</span>
+                    </dd>
+                  </dl>
                   <p className="color-brand">{order.status}</p>
                 </div>
               </a>

81053e0c51b587da6f48de64e6bf9588bec14d56
Simon Emanuel Schmid <simon@schmid.io>
2020-09-11 17:42:17 +0200

    First steps with storybook. Failing.

--- storefront/.storybook/main.js
+++ storefront/.storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-  stories: ['../components/**/*.stories.@(ts|tsx|js|jsx|mdx)'],
+  stories: ['../elements/**/*.stories.@(ts|tsx|js|jsx|mdx)'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
 }
---
+++ storefront/elements/Button.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Button from './Button'
+// import { action } from '@storybook/addon-actions'
+
+export default { title: 'Button' }
+
+export const test = () => (
+  <Button>Test</Button>
+)
+

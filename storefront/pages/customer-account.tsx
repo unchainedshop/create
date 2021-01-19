ad1ea101784a7f39d042fb8ca179d9c5a0bcf8df
mikearaya <mikaelaraya12@gmail.com>
2021-01-19 23:58:21 +0300

    Update signup form

--- storefront/pages/customer-account.tsx
+++ storefront/pages/customer-account.tsx
@@ -1,12 +1,15 @@
+import useUserQuery from '../modules/auth/hooks/useUserQuery';
 import Header from '../modules/layout/components/Header';
 
 const Account = () => {
+  const { user } = useUserQuery();
   return (
     <div className="container">
       <Header />
       <div className="row">
         <div className="col-md-8 offset-md-2">
           <h1>Customer account</h1>
+          <code> {JSON.stringify(user)} </code>
         </div>
       </div>
     </div>

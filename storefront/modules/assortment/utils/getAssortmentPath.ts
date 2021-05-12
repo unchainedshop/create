2f24ce3e1a853aa90917f91bb39f6b90f332691c
Mikael Araya <mikaelaraya12@gmail.com>
2021-05-12 23:18:53 +0300

    Optimize breadcrumb generation

--- storefront/modules/assortment/utils/getAssortmentPath.ts
+++ storefront/modules/assortment/utils/getAssortmentPath.ts
@@ -1,11 +1,8 @@
 const getAssortmentPath = (assortmentPaths = [], pathFromRoot = 'shop') => {
   const pathArr = [];
 
-  const recuresive = (
-    paths = assortmentPaths,
-    rootPath = pathFromRoot,
-  ): [{ id: string; slug: string; title: string }] | [] => {
-    if (paths.length === 0) return [];
+  const recuresive = (paths = assortmentPaths, rootPath = pathFromRoot) => {
+    if (paths.length === 0) return;
 
     const [currentLink, ...nextLink] = paths;
 
@@ -14,10 +11,7 @@ const getAssortmentPath = (assortmentPaths = [], pathFromRoot = 'shop') => {
       title: currentLink.link.parent.texts.title,
       slug: `${rootPath}/${currentLink.link.parent.texts.slug}`,
     });
-    return recuresive(
-      nextLink,
-      `${rootPath}/${currentLink.link.parent.texts.slug}`,
-    );
+    recuresive(nextLink, `${rootPath}/${currentLink.link.parent.texts.slug}`);
   };
   recuresive(assortmentPaths, pathFromRoot);
   return pathArr;

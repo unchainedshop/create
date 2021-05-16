e5a480ec878e4520a3f39e585a9f76dcc78f9aa4
Mikael Araya <mikaelaraya12@gmail.com>
2021-05-16 18:59:50 +0300

    Update order confirmation page to show orderNumber and date

--- storefront/pages/thank-you.tsx
+++ storefront/pages/thank-you.tsx
@@ -1,23 +1,41 @@
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 
+import useOrderDetail from '../modules/orders/hooks/useOrderDetail';
+
+import Header from '../modules/layout/components/Header';
 import Footer from '../modules/layout/components/Footer';
+import formatDate from '../modules/common/utils/formatDate';
 
 const ThankYou = () => {
+  const router = useRouter();
+  if (!router.query.orderId) return '';
+  const { order } = useOrderDetail({
+    orderId: router.query?.orderId,
+  });
+
   return (
-    <div className="container">
-      <Link href="/">
-        <a className="color-brand">
-          <h3 className="my-2 mr-2">Unchained</h3>
-        </a>
-      </Link>
-      <div className="row">
-        <div className="col-md-8 offset-md-2">
-          <h1>thank you for your order</h1>
-          <p>We will...</p>
+    <>
+      <Header />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 offset-md-2">
+            <h1>Thank You for Placing this Order with Us!</h1>
+            <p>
+              It has reached us and an email with the order placement
+              confirmation is on its way. To avoid any potential
+              miscommunication, please check your spam, perhaps the email landed
+              there.
+            </p>
+            <h4>Your Order Number is: {order?.orderNumber}</h4>
+            <p>
+              The Date you placed the order is: {formatDate(order?.created)}
+            </p>
+          </div>
         </div>
       </div>
+
       <Footer />
-    </div>
+    </>
   );
 };
 

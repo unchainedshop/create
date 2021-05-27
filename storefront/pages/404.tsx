e0448d8137db89ff2296b8b18cb0868618504a10
Mikael Araya <mikaelaraya12@gmail.com>
2021-05-27 10:36:27 +0300

    Add redirection to Not found page when item not found

--- storefront/pages/404.tsx
+++ storefront/pages/404.tsx
@@ -5,7 +5,7 @@ import Header from '../modules/layout/components/Header';
 import Footer from '../modules/layout/components/Footer';
 import MetaTags from '../modules/common/components/MetaTags';
 
-const NotFound = () => {
+const NotFound = ({ action = '' }) => {
   return (
     <>
       <MetaTags title="404: Not Found" />
@@ -13,8 +13,7 @@ const NotFound = () => {
       <div className="container text-center text-danger p-3 p-md-5">
         <div className="p-lg-5 mb-4">
           <h1 className="font-weight-bolder font-dax-ot-regular">
-            {' '}
-            404: Requested Page not found
+            404: Requested {action || 'Page'} not found
           </h1>
           <div className="mb-5">
             <div className="text-center">

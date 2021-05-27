03bbc3b20574686efa634204bc063790a75a3fba
Mikael Araya <mikaelaraya12@gmail.com>
2021-05-27 10:30:24 +0300

    Add 404 page

---
+++ storefront/pages/404.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Link from 'next/link';
+
+import Header from '../modules/layout/components/Header';
+import Footer from '../modules/layout/components/Footer';
+import MetaTags from '../modules/common/components/MetaTags';
+
+const NotFound = () => {
+  return (
+    <>
+      <MetaTags title="404: Not Found" />
+      <Header />
+      <div className="container text-center text-danger p-3 p-md-5">
+        <div className="p-lg-5 mb-4">
+          <h1 className="font-weight-bolder font-dax-ot-regular">
+            {' '}
+            404: Requested Page not found
+          </h1>
+          <div className="mb-5">
+            <div className="text-center">
+              <p>Sorry, the page you were looking for was not found! </p>
+              <Link href="/">
+                <a className="button button--primary">Back to home</a>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div />
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default NotFound;

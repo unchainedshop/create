15b15206404b571300af2b0ed60d23f13066abb4
vedi <vedran@unchained.shop>
2021-06-08 14:08:35 +0200

    delete unused icons

--- storefront/modules/cart/components/CartItem.tsx
+++ storefront/modules/cart/components/CartItem.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
-import getMediaUrl from '../../common/utils/getMediaUrl';
 
+import getMediaUrl from '../../common/utils/getMediaUrl';
+import Icon from '../../common/components/Icon';
 import renderPrice from '../../common/utils/renderPrice';
 import useRemoveCartItem from '../hooks/useRemoveCartItem';
 import useUpdateCartItemMutation from '../hooks/useUpdateCartItem';
@@ -75,16 +76,7 @@ const CartItem = ({ _id, quantity, product, total }) => {
           aria-label="remove cart item"
           onClick={() => removeCartItem({ itemId: _id })}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            height="16px"
-            fill="currentColor"
-          >
-            <title>Remove item</title>
-            <path d="M.249,12A11.751,11.751,0,1,0,12,.249,11.765,11.765,0,0,0,.249,12Zm21,0A9.251,9.251,0,1,1,12,2.749,9.262,9.262,0,0,1,21.251,12Z" />
-            <path d="M8.833,7.066A1.25,1.25,0,1,0,7.065,8.834l2.99,2.989a.25.25,0,0,1,0,.354l-2.99,2.99a1.25,1.25,0,1,0,1.768,1.767l2.989-2.989a.249.249,0,0,1,.354,0l2.99,2.989a1.249,1.249,0,1,0,1.767-1.767l-2.989-2.99a.25.25,0,0,1,0-.354l2.989-2.989a1.25,1.25,0,1,0-1.767-1.768l-2.99,2.99a.251.251,0,0,1-.354,0Z" />
-          </svg>
+          <Icon className="icon--small" icon="bin-2-alternate" />
         </button>
       </div>
       <div className="d-flex justify-content-between align-items-center flex-wrap">
--- storefront/modules/cart/components/SideCart.jsx
+++ storefront/modules/cart/components/SideCart.jsx
@@ -6,6 +6,7 @@ import { useIntl } from 'react-intl';
 import renderPrice from '../../common/utils/renderPrice';
 import useUser from '../../auth/hooks/useUser';
 import { CartContext } from '../CartContext';
+import Icon from '../../common/components/Icon';
 import CartItem from './CartItem';
 
 const SideCart = ({ isOpen }) => {
@@ -42,30 +43,19 @@ const SideCart = ({ isOpen }) => {
       {!user?.cart?.items.length ? (
         <>
           <div
-            className={`side-cart-container p-2 text-center ${
+            className={`side-cart-container py-3 px-2 text-center ${
               isOpen ? 'open' : ''
             }`}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              height="24px"
-              fill="currentColor"
-              className="mt-4"
-            >
-              <title>{intl.formatMessage({ id: 'empty_cart' })}</title>
-              <circle cx="14.531" cy="21.125" r="2.25" />
-              <circle cx="4.531" cy="21.125" r="2.25" />
-              <path d="M22.469.625H19.407a1.756,1.756,0,0,0-1.735,1.519L15.9,15.408a.251.251,0,0,1-.248.217H1.531a1.25,1.25,0,1,0,0,2.5h15a1.758,1.758,0,0,0,1.736-1.519L20.035,3.342a.249.249,0,0,1,.247-.217h2.187a1.25,1.25,0,0,0,0-2.5Z" />
-            </svg>
+            <Icon icon="shopping-bag-sad" />
             <p>
               {intl.formatMessage({ id: 'no_product_in_cart' })}{' '}
               <Link href="/shop">
                 <a
                   onClick={() => context.toggleCart(false)}
-                  className="link color-brand"
+                  className="link"
                 >
-                  {intl.formatMessage({ id: 'products' })}
+                  {intl.formatMessage({ id: 'shop' })}
                 </a>
               </Link>
             </p>
@@ -86,16 +76,7 @@ const SideCart = ({ isOpen }) => {
                   className="no-button p-2 position-absolute close-cart-button"
                   onClick={() => context.toggleCart()}
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    height="10px"
-                    fill="currentColor"
-                    opacity=".5"
-                  >
-                    <title>{intl.formatMessage({ id: 'close' })}</title>
-                    <path d="M14.3,12.179a.25.25,0,0,1,0-.354l9.263-9.262A1.5,1.5,0,0,0,21.439.442L12.177,9.7a.25.25,0,0,1-.354,0L2.561.442A1.5,1.5,0,0,0,.439,2.563L9.7,11.825a.25.25,0,0,1,0,.354L.439,21.442a1.5,1.5,0,0,0,2.122,2.121L11.823,14.3a.25.25,0,0,1,.354,0l9.262,9.263a1.5,1.5,0,0,0,2.122-2.121Z" />
-                  </svg>
+                  <Icon className="icon--small" icon="close" />
                 </button>
               </div>
               <h3 className="p-3 m-0 h4 text-center d-block">
@@ -112,7 +93,7 @@ const SideCart = ({ isOpen }) => {
                       className="link"
                       href="#"
                     >
-                      {intl.formatMessage({ id: 'shop' })}
+                      {intl.formatMessage({ id: 'shop' })}.
                     </a>
                   </Link>
                 </p>

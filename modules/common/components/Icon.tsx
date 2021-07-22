c051881259091f6f28d6258cc5e4877ee1fc8f6e
Pascal Kaufmann <kaufmae@me.com>
2021-07-22 11:43:08 +0200

    Switch to new template generation

--- modules/common/components/Icon.tsx
+++
@@ -1,9 +0,0 @@
-const Icon = ({ icon, className = '' }) => {
-  return (
-    <span className={`icon ${className}`}>
-      <img src={`/static/img/icon-streamline/${icon}.svg`} />
-    </span>
-  );
-};
-
-export default Icon;
--- modules/common/utils/getCurrentDomain.ts
+++
@@ -1,9 +0,0 @@
-import getConfig from 'next/config';
-
-const {
-  publicRuntimeConfig: { FRONTEND_URL },
-} = getConfig();
-
-const getCurrentDomain = () => FRONTEND_URL;
-
-export default getCurrentDomain;
